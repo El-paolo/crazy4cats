7e094b4b7a2d41d5d86857c549e4aa2eb9ce1207
El-paolo <paolocordovacontacto@gmail.com>
2023-08-23 02:27:18 -0400

    added the reactions to the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :users, through: :reactions
 
+  Kinds = %w[like dislike neutral].freeze
+  KindsSpanish = {"like" => "Me gusta", "dislike" => "No me gusta", "neutral" => "Neutral"}.freeze
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
+  post '/new_user_reaction', to: 'reactions#new_user_reaction', as:
+  'new_user_reaction'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

e553528def60745cef83e46e2b6eff9f37c88039
El-paolo <paolocordovacontacto@gmail.com>
2023-08-22 23:50:52 -0400

    added the relations to the models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :reactions, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :users, through: :reactions
+
 end
--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,4 +1,11 @@
 class Reaction < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :kind, acceptance: {
+    accept: %w[ like dislike neutral ],
+    }
+    def self.kinds
+      %w[like dislike boring neutral uninterested interested]
+    end
 end

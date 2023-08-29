c863e069c3530c91ba7fa06135ec04e4ba4c2fb3
El-paolo <paolocordovacontacto@gmail.com>
2023-08-29 03:07:20 -0400

    added users, comments and posts from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+# Crear 10 usuarios con datos aleatorios
+i = 0
+10.times do
+    User.create(email: "test#{i}@gmail", password: "asdasdasd",
+    password_confirmation: "asdasdasd", name:
+    Faker::Name.name)
+    i+= 1
+    end
+  
+k=0
+10.times do
+    Post.create(
+      
+      title: Faker::Book.title,
+      description: Faker::Lorem.paragraphs(number: 2).join("\n\n"),
+      image: "https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg",
+      user_id: k+= 1
+      # Agrega más campos aquí según lo necesites
+    )
+  end
+
+posts = Post.all
+users = User.all
+10.times do
+    Comment.create(content: Faker::Lorem.paragraph_by_chars(number: 200,
+    supplemental: false), post_id: posts.sample.id, user_id:
+    users.sample.id)
+    end
